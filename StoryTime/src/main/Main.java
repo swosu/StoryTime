27877ca12efe8ac8e2fc20e23fd7a83935a18ced
evertj <evertj@EvertJ-PC3.admin.swosu.edu>
2020-11-16 14:37:24 -0600

    add game set up class

--- StoryTime/src/main/Main.java
+++ StoryTime/src/main/Main.java
@@ -1,12 +1,20 @@
 package main;
 
+import java.util.Random;
+import java.util.Scanner;
+import gameSetUp.GameSetUp;
+
 import introduction.Greetings;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Random rnd = new Random(System.currentTimeMillis());
 		System.out.println("Program Begin");
 		Greetings.printGreeting();
+		GameSetUp Cs2Game = new GameSetUp();
+		Cs2Game.setUpStoryTime(input, rnd);
 		System.out.println("Program End");
 	}
 
