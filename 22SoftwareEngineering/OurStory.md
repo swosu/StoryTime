fd3012be5fce8327242e83ec1043bc441e770ee6
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-01-20 09:27:07 -0600

    added a cheat sheet and the start of story time.

---
+++ 22SoftwareEngineering/OurStory.md
@@ -0,0 +1,1 @@
+# 2022 Software Engineering
--- StoryTime/src/introduction/Greetings.java
+++
@@ -1,9 +0,0 @@
-package introduction;
-
-public class Greetings {
-	public static void printGreeting() {
-		System.out.println("Greetings.");
-		System.out.println("This is a game that we play to practice coding and learning together.");
-		System.out.println("The main goal of this project is to have a little fun and learn about source code management.");
-	}
-}
